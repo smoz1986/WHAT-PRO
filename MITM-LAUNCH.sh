70ad2872e73103b438cb073aaf54c24eb687461b
smoz1986 <smoz1986@users.noreply.github.com>
2016-02-27 13:26:20 +0000

    Update MITM-LAUNCH.sh

--- MITM-LAUNCH.sh
+++ MITM-LAUNCH.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-gnome-terminal -e ./MITM-ETTERCAP.sh --working-directory=/root/WHAT-PRO
+gnome-terminal -e ./MITM-ETTERCAP.sh --working-directory=/root/WHAT-PRO && gnome-terminal -e ./SSLSTRIP.sh —working-directory=/root/WHAT-PRO
