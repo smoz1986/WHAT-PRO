6e636527f5de95f5df56001aa92c46ffd9aa3652
smoz1986 <smoz1986@users.noreply.github.com>
2016-02-28 23:54:40 +0000

    Update WHAT-installer.sh

--- WHAT-installer.sh
+++ WHAT-installer.sh
@@ -6,8 +6,11 @@ echo installing iSniff-GPS
 apt-get update && apt-get upgrade -y
 apt-get install python-pip python-setuptools git git-core gpsd gpsd-clients python-m2crypto libgcrypt11-dev libnl-3-dev libnl-genl-3-dev libxml-libxml-perl libdbi-perl libdbd-sqlite3-perl -y
 rm -r /etc/ettercap/etter.conf
+git clone https://github.com/byt3bl33d3r/sslstrip2.git
+git clone https://github.com/LeonardoNve/dns2proxy.git
+cd sslstrip2 && python setup.py install
 cp /root/WHAT-PRO/etter.conf /etc/ettercap/etter.conf
-mkdir geo-tools && cd geo-tools
+cd .. && mkdir geo-tools && cd geo-tools
 git clone https://github.com/hubert3/iSniff-GPS.git
 cd iSniff-GPS
 pip install -U -r requirements.txt
