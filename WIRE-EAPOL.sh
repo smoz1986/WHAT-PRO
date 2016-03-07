ed394ab231877e206716c63106a65960b09b76df
smoz1986 <smoz1986@users.noreply.github.com>
2016-03-07 23:04:42 +0000

    Delete WIRE-EAPOL.sh

--- WIRE-EAPOL.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# WIRESHARK EAPOL CAPTURE
-
-echo Starting Wireshark to confirm EAPOL capture of target AP...just agree to any dialogue that comes up. Close this window once Wireshark starts
-sudo wireshark -k -Y 'eapol' -i wlan0mon
