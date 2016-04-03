e36cc6b9618a73f58f4be8db8462328de734db44
smoz1986 <smoz1986@users.noreply.github.com>
2016-04-03 23:02:16 +0100

    Update OVPN.sh

--- OVPN.sh
+++ OVPN.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
 service openvpn start
+cd /etc/openvpn
+echo What is the name of your OpenVPN file? - input the full file name i.e. blah.ovpn or blah.conf. Also make sure that your OpenVPN files are in the /etc/openvpn directory before continuing...
+read -p 'OpenVPN file: ' ovpnvar
+openvpn $ovpnvar
 echo OpenVPN has started please close this terminal!
